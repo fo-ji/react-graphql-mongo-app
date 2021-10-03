0ddc96f5cc05ba883d02fc84e4e091912f9535ad
fo-ji <fumiya1753@gmail.com>
2021-10-03 14:24:46 +0900

    set mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const graphqlHTTP = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
+const mongoose = require('mongoose')
 const app = express()
 
-app.use('/graphql', graphqlHTTP)
+require('dotenv').config()
+const env = process.env
+
+mongoose.connect(env.MONGO_DB_URL)
+mongoose.connection.once('open', () => {
+  console.log('db connected')
+})
+
+app.use('/graphql', graphqlHTTP({}))
 app.listen(4000, () => {
   console.log('listening port 4000')
 })
