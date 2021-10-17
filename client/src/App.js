8de6b43121f3ba4a02317b871faab4f5b3e71e40
fo-ji <fumiya1753@gmail.com>
2021-10-17 16:14:50 +0900

    implement MovieList

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import { Col, Container, Row } from 'reactstrap'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 import Header from './components/Header'
 import SideNav from './components/SideNav'
 import MovieList from './components/MovieList'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Container>
-        <Row>
-          <Col xs={12} sm={4}>
-            <SideNav />
-          </Col>
-          <Col xs={12} sm={8}>
-            <MovieList />
-          </Col>
-        </Row>
-      </Container>
+      <ApolloProvider client={client}>
+        <Container>
+          <Row>
+            <Col xs={12} sm={4}>
+              <SideNav />
+            </Col>
+            <Col xs={12} sm={8}>
+              <MovieList />
+            </Col>
+          </Row>
+        </Container>
+      </ApolloProvider>
     </div>
   )
 }
--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const mongoose = require('mongoose')
 const schema = require('./schema/schema')
+const cors = require('cors')
 const app = express()
 
 require('dotenv').config()
 const env = process.env
 
+app.use(cors())
 mongoose.connect(env.MONGO_DB_URL)
 mongoose.connection.once('open', () => {
   console.log('db connected')
