91df9f93333d81d1552c0e8b74a7123125a2ee23
fo-ji <fumiya1753@gmail.com>
2021-10-18 23:18:29 +0900

    implement SideNav form

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,10 +1,23 @@
 import { Button, Card, CardBody, CardHeader, Form, FormGroup } from 'reactstrap'
-import { useQuery } from '@apollo/client'
+import { useQuery, useMutation } from '@apollo/client'
+import { useForm } from 'react-hook-form'
 
-import { DIRECTOR_LIST } from '../queries/queries'
+import { DIRECTOR_LIST, ADD_MOVIE } from '../queries/queries'
 
 function SideNav() {
   const { data } = useQuery(DIRECTOR_LIST)
+  const [addMovie] = useMutation(ADD_MOVIE)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = ({ movieName, movieGenre, directorId }) => {
+    addMovie({
+      variables: { name: movieName, genre: movieGenre, directorId },
+    })
+  }
 
   return (
     <div>
@@ -37,20 +50,34 @@ function SideNav() {
       <Card className="mt-4">
         <CardHeader>映画作品</CardHeader>
         <CardBody>
-          <Form>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <FormGroup>
               <input
                 type="text"
                 className="form-control"
-                name="movieName"
                 placeholder="タイトル"
+                {...register('movieName')}
               />
             </FormGroup>
             <FormGroup>
-              <select type="select" className="form-control" name="directorId">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="ジャンル"
+                {...register('movieGenre')}
+              />
+            </FormGroup>
+            <FormGroup>
+              <select
+                type="select"
+                className="form-control"
+                {...register('directorId')}
+              >
                 {data &&
                   data.directors.map(({ id, name }) => (
-                    <option key={id}>{name}</option>
+                    <option key={id} value={id}>
+                      {name}
+                    </option>
                   ))}
               </select>
             </FormGroup>
